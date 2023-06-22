e0c42b9efdc9e06daa50f3d433ee4d17ad5e872e
Quentayro <quentayro+github@gmail.com>
2023-06-22 16:09:17 +0200

    Switching laptop

--- src/pages/Music/MusicArtist.tsx
+++ src/pages/Music/MusicArtist.tsx
@@ -1,8 +1,38 @@
+import axios from 'axios';
+import { useContext, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { Context } from 'utils/context';
+import { getMusicArtistEndpoint } from 'utils/endpoints';
+import { errorNoServerResponseText, errorText } from 'utils/texts';
 
 export const MusicArtist = () => {
-	const params = useParams();
-	console.log(params);
+	const context = useContext(Context);
+	const { openErrorNotification } = context;
+
+	const { spotifyId } = useParams();
+
+	useEffect(() => {
+		requestGetArtist();
+	}, []);
+
+	const requestGetArtist = async () => {
+		try {
+			const response = await axios.get(getMusicArtistEndpoint, { params: { spotifyId: spotifyId } });
+			console.log('response data : ', response.data);
+		} catch (error: any) {
+			const { code } = error;
+
+			const errorMessage = () => {
+				switch (code) {
+					case 'ERR_NETWORK':
+						return errorNoServerResponseText;
+					default:
+						return `${errorText} : ${code}`;
+				}
+			};
+			openErrorNotification(errorMessage());
+		}
+	};
 
 	return <>WIP</>;
 };
--- src/utils/endpoints.ts
+++ src/utils/endpoints.ts
@@ -4,5 +4,6 @@ const baseUrl = 'http://localhost:8080';
 const musicUrl = `${baseUrl}/music`;
 
 export const getMusicArtistsEndpoint = `${musicUrl}/getArtists`;
+export const getMusicArtistEndpoint = `${musicUrl}/getArtist`;
 export const postMusicArtistEndpoint = `${musicUrl}/postArtist`;
 export const postMusicGenreDisplayTextEndpoint = `${musicUrl}/postGenreDisplayText`;
